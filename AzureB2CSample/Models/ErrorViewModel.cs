24a9b7680bf9df1573226dac958215d70b728021
Jagannathan <ijaganna@amazon.com>
2018-09-07 14:48:51 -0400

    Add project files.

---
+++ AzureB2CSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureB2CSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
